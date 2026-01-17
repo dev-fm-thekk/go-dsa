0122893d11caf2adc8698d6077c1fa5e48503966
Abhiram-advop <abhiramajithr@gmail.com>
2026-01-17 23:39:21 +0530

    feat: update Header and Hero components, add MobileStatsCard, and remove unused SVGs

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Hero from "./components/Hero";
+import MobileStatsCard from "./components/MobileStatsCard";
 
 export default function Home() {
   return (
     <div>
-      <Hero />
+      <section>
+        <div className="mt-4 mb-2">
+          <MobileStatsCard />
+        </div>
+        <Hero />
+      </section>
     </div>
   );
 }
